fb68e3469cea655f26f22c050a8885f618630e88
nziokaivy <nziokaivy@gmail.com>
2019-03-22 08:51:11 +0300

    create super user, add urls file to app, create first view and template

--- hood/views.py
+++ hood/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 
 # Create your views here.
+
+def home(request):
+    
+    return render(request, 'index.html')
